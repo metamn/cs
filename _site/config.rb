499a56f61720a24319efbdeaf3b4e2cdd4137b3c
metamn <bartus.csongor@gmail.com>
2013-07-21 12:14:52 +0300

    Trying relative assets path

--- _site/config.rb
+++ _site/config.rb
@@ -1,7 +1,7 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "http://metamn.github.io/cs/"
 css_dir = "assets"
 sass_dir = "assets"
 images_dir = "assets"
@@ -13,7 +13,7 @@ fonts_dir = "assets"
 output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
